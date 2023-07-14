7d8a78d525adea80b5b1fc876256b1bc419fbd86
PurimDEVY <91385716+PurimDEVY@users.noreply.github.com>
2023-07-14 10:36:30 +0700

    init get post wordpress

--- web-blog/src/app/page.tsx
+++ web-blog/src/app/page.tsx
@@ -2,7 +2,7 @@ import { getPosts } from "@/service/api_service/blogapi"
 
 export default async function Home() {
   const data = await getPosts()
-  // console.log(data)
+  console.log(data)
   return (
     <h1>hello home</h1>
   )
--- web-blog/src/service/api_service/blogapi.tsx
+++ web-blog/src/service/api_service/blogapi.tsx
@@ -1,6 +1,7 @@
 import { fetchAPI } from "./common";
 
 export async function getPosts(first = 10) {
+  console.log("call get post")
     const data = await fetchAPI(
       `query FetchPosts($first: Int = 10) {
           posts(first: $first) {
